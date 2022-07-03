a7d875e5462da359ca66a68a90445ef180b87119
Maciej Nazarczuk <maciej.nazarczuk@gmail.com>
2022-07-03 21:33:25 +0200

    changed how images are pushed

--- src/main/java/com/mnazarczuk/imgservice/images/ImgRestController.groovy
+++ src/main/java/com/mnazarczuk/imgservice/images/ImgRestController.groovy
@@ -3,7 +3,8 @@ package com.mnazarczuk.imgservice.images;
 import groovy.json.JsonSlurper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -12,28 +13,43 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import org.springframework.http.MediaType
 
 
 @RestController()
 public class ImgRestController {
 
-    @GetMapping("/api/image")
-    public String test123(@RequestParam  String code, @RequestParam  String city) throws IOException, InterruptedException {
+    @GetMapping(
+            value = "/api/image",
+            produces = MediaType.APPLICATION_OCTET_STREAM_VALUE
+    )
+    @ResponseBody
+    byte[] downloadFile(@RequestParam String code, @RequestParam String city) throws IOException, InterruptedException {
         System.out.println(code);
         System.out.println(city);
         String query = "$city $code airport"
         def encoded = URLEncoder.encode(query, StandardCharsets.UTF_8)
         HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create("https://bing-image-search1.p.rapidapi.com/images/search?q=" + encoded))
-            .header("X-RapidAPI-Key", "e8a32ef5cbmshaa1803051b3af29p15ce7fjsn4f04004a4a56")
-            .header("X-RapidAPI-Host", "bing-image-search1.p.rapidapi.com")
-            .method("GET", HttpRequest.BodyPublishers.noBody())
-            .build();
+                .uri(URI.create("https://bing-image-search1.p.rapidapi.com/images/search?q=" + encoded))
+                .header("X-RapidAPI-Key", "e8a32ef5cbmshaa1803051b3af29p15ce7fjsn4f04004a4a56")
+                .header("X-RapidAPI-Host", "bing-image-search1.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
 
         def object = new JsonSlurper().parseText(response.body())
         def url = object.value[0].contentUrl
         System.out.println(url);
-        return  url;
+
+        HttpRequest request2 = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<byte[]> imgResponse = HttpClient.newHttpClient().send(request2, HttpResponse.BodyHandlers.ofByteArray());
+
+
+        return imgResponse.body()
     }
+
 }
